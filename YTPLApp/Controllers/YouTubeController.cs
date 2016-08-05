2d29fae121ef70569ae2a7a8471566a05700686b
Aarsh <Aarsh@AARSH-PC>
2016-08-05 00:04:26 -0400

    Add project files.

---
+++ YTPLApp/Controllers/YouTubeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using YTPLApp.Domain;
+
+namespace YTPLApp.Controllers
+{
+    public class YouTubeController : ApiController
+    {
+        public string Get()
+        {
+            return "this is a test";
+        }
+
+        public string post()
+        {
+            return "you posted to an empty method";
+        }
+
+        public string PostSingle(string videoId)
+        {
+            YouTube yt = new YouTube();
+            yt.Test();
+            return "you sent: " + videoId;
+        }
+
+        public string[] PostPlaylist(string playlistId)
+        {
+            return new string[] { "http://google.com", "http://bing.con" };
+        }
+    }
+}
---
+++ YTPLApp/Domain/YouTube.cs
@@ -0,0 +1,33 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Google.Apis.YouTube.v3.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YTPLApp.Domain
+{
+    public class YouTube
+    {
+        public void Test()
+        {
+            List<string> myResults = new List<string>();
+
+            YouTubeService youtube = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApplicationName = "youtube-test",
+                ApiKey = "AIzaSyDafGzPwgo6ycE7OQ28wDO_lOFoLWZMluk",
+            });
+            SearchResource.ListRequest listRequest = youtube.Search.List("snippet");
+            listRequest.Q = "Loeb Pikes Peak";
+            listRequest.MaxResults = 5;
+            listRequest.Type = "video";
+            SearchListResponse resp = listRequest.Execute();
+            foreach (SearchResult result in resp.Items)
+            {
+                myResults.Add(result.Snippet.Title);
+            }
+        }
+    }
+}
