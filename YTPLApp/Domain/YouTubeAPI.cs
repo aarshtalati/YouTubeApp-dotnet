e2c30ef676adc29652ff7f5b3c3885590e629b9a
ablaze8 <aarshstalati1989@gmail.com>
2016-08-06 02:28:02 -0400

    cosmetic changes

--- YTPLApp/Domain/YouTubeAPI.cs
+++ YTPLApp/Domain/YouTubeAPI.cs
@@ -20,164 +20,174 @@ using YTPLApp.Model;
 
 namespace YTPLApp.Domain
 {
-	public class YouTubeAPI
-	{
-		private static YouTubeService youtubeService;
+    public class YouTubeAPI
+    {
+        private static YouTubeService youtubeService;
 
-		public YouTubeAPI()
-		{
-			var filePath = HttpContext.Current.Server.MapPath("~/GoogleKeys.json");
-			if (filePath.Contains("api\\YouTube\\"))
-			{
-				filePath = filePath.Replace("api\\YouTube\\", "");
-			}
+        public YouTubeAPI()
+        {
+            var filePath = HttpContext.Current.Server.MapPath("~/GoogleKeys.json");
+            if (filePath.Contains("api\\YouTube\\"))
+            {
+                filePath = filePath.Replace("api\\YouTube\\", "");
+            }
 
-			JObject googleKeys = new JObject();
-			using (StreamReader file = File.OpenText(filePath))
-			{
-				using (var reader = new JsonTextReader(file))
-				{
-					googleKeys = (JObject)JToken.ReadFrom(reader);
-				}
-			}
+            JObject googleKeys = new JObject();
+            using (StreamReader file = File.OpenText(filePath))
+            {
+                using (var reader = new JsonTextReader(file))
+                {
+                    googleKeys = (JObject)JToken.ReadFrom(reader);
+                }
+            }
 
-			youtubeService = new YouTubeService(new BaseClientService.Initializer()
-			{
-				ApplicationName = "youtube-test",
-				ApiKey = googleKeys["ServerApiKey"].ToString(),
-			});
-
-
+            youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            {
+                ApplicationName = "youtube-test",
+                ApiKey = googleKeys["ServerApiKey"].ToString(),
+            });
 
 
 
-			UserCredential credential;
-
-			//var filePath = HttpContext.Current.Server.MapPath("client_secrets.json");
-			//if (filePath.Contains("api\\YouTube\\"))
-			//{
-			//    filePath = filePath.Replace("api\\YouTube\\", "");
-			//}
-
-			//var folder = HttpContext.Current.Server.MapPath("~/App_Data/MyGoogleStorage");
 
 
-			//using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-			//{
-			//    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
-			//        GoogleClientSecrets.Load(stream).Secrets,
-			//        // This OAuth 2.0 access scope allows for read-only access to the authenticated 
-			//        // user's account, but not other types of account access.
-			//        new[] { YouTubeService.Scope.YoutubeReadonly },
-			//        "user",
-			//        CancellationToken.None,
-			//        new FileDataStore(folder)
-			// ).Result;
+            UserCredential credential;
+
+            //var filePath = HttpContext.Current.Server.MapPath("client_secrets.json");
+            //if (filePath.Contains("api\\YouTube\\"))
+            //{
+            //    filePath = filePath.Replace("api\\YouTube\\", "");
+            //}
+
+            //var folder = HttpContext.Current.Server.MapPath("~/App_Data/MyGoogleStorage");
 
 
-			//}
-
-			//var youtubeService = new YouTubeService(new BaseClientService.Initializer()
-			//{
-			//    HttpClientInitializer = credential,
-			//    ApplicationName = this.GetType().ToString()
-			//});
-
-
-
-		}
-
-
-		public void Test()
-		{
-			List<string> myResults = new List<string>();
+            //using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            //{
+            //    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+            //        GoogleClientSecrets.Load(stream).Secrets,
+            //        // This OAuth 2.0 access scope allows for read-only access to the authenticated 
+            //        // user's account, but not other types of account access.
+            //        new[] { YouTubeService.Scope.YoutubeReadonly },
+            //        "user",
+            //        CancellationToken.None,
+            //        new FileDataStore(folder)
+            // ).Result;
 
 
-			SearchResource.ListRequest listRequest = youtubeService.Search.List("snippet");
-			listRequest.Q = "Loeb Pikes Peak";
-			listRequest.MaxResults = 5;
-			listRequest.Type = "video";
-			SearchListResponse resp = listRequest.Execute();
-			foreach (SearchResult result in resp.Items)
-			{
-				myResults.Add(result.Snippet.Title);
-			}
-		}
+            //}
+
+            //var youtubeService = new YouTubeService(new BaseClientService.Initializer()
+            //{
+            //    HttpClientInitializer = credential,
+            //    ApplicationName = this.GetType().ToString()
+            //});
 
 
-		public IEnumerable<YouTubeVideo> GetByUserId(string id = "EllieGouldingVEVO")
-		{
-			var myResults = new List<YouTubeVideo>();
-			var channelsListRequest = youtubeService.Channels.List("contentDetails");
-			
-			channelsListRequest.ForUsername = id;
-			var channelsListResponse = channelsListRequest.ExecuteAsync().Result;
+
+        }
 
 
-			foreach (var channel in channelsListResponse.Items)
-			{
-				// From the API response, extract the playlist ID that identifies the list
-				// of videos uploaded to the authenticated user's channel.
-				var uploadsListId = channel.ContentDetails.RelatedPlaylists.Uploads;
-
-				var nextPageToken = "";
-				while (nextPageToken != null)
-				{
-					var playlistItemsListRequest = youtubeService.PlaylistItems.List("snippet");
-					playlistItemsListRequest.PlaylistId = uploadsListId;
-					playlistItemsListRequest.MaxResults = 20;
-					playlistItemsListRequest.PageToken = nextPageToken;
-
-					// Retrieve the list of videos uploaded to the authenticated user's channel.
-					var playlistItemsListResponse = playlistItemsListRequest.ExecuteAsync().Result;
-
-					foreach (var playlistItem in playlistItemsListResponse.Items)
-					{
-						myResults.Add(new YouTubeVideo
-						{
-							Description = playlistItem.Snippet.Description,
-							Id = playlistItem.Snippet.ResourceId.VideoId,
-							PublishDate = playlistItem.Snippet.PublishedAt,
-							Title = playlistItem.Snippet.Title,
-						});
-					}
-					nextPageToken = playlistItemsListResponse.NextPageToken;
-				}
-			}
-			return myResults.AsEnumerable();
-		}
+        public void Test()
+        {
+            List<string> myResults = new List<string>();
 
 
-		public IEnumerable<YouTubeVideo> GetByPlayListId(string id)
-		{
-			var playListId = id;
-			var nextPageToken = "";
-			var myResults = new List<YouTubeVideo>();
-			PlaylistItemsResource.ListRequest playlistItemsListRequest = null;
+            SearchResource.ListRequest listRequest = youtubeService.Search.List("snippet");
+            listRequest.Q = "Loeb Pikes Peak";
+            listRequest.MaxResults = 5;
+            listRequest.Type = "video";
+            SearchListResponse resp = listRequest.Execute();
+            foreach (SearchResult result in resp.Items)
+            {
+                myResults.Add(result.Snippet.Title);
+            }
+        }
 
-			while (nextPageToken !=null)
-			{
-				playlistItemsListRequest = youtubeService.PlaylistItems.List("snippet");
-				playlistItemsListRequest.PlaylistId = playListId;
-				playlistItemsListRequest.MaxResults = 10;
-				playlistItemsListRequest.PageToken = nextPageToken;
 
-				var playlistItemsListResponse = playlistItemsListRequest.ExecuteAsync().Result;
+        public IEnumerable<YouTubeVideo> GetByUserId(string id = "EllieGouldingVEVO")
+        {
+            var myResults = new List<YouTubeVideo>();
+            var channelsListRequest = youtubeService.Channels.List("contentDetails");
 
-				foreach (var playlistItem in playlistItemsListResponse.Items)
-				{
-					myResults.Add(new YouTubeVideo
-					{
-						Description = playlistItem.Snippet.Description,
-						Id = playlistItem.Snippet.ResourceId.VideoId,
-						PublishDate = playlistItem.Snippet.PublishedAt,
-						Title = playlistItem.Snippet.Title,
-					});
-				}
+            channelsListRequest.ForUsername = id;
+            var channelsListResponse = channelsListRequest.ExecuteAsync().Result;
 
-				nextPageToken = playlistItemsListResponse.NextPageToken; 
-			}
-			return myResults.AsEnumerable();
-		}
-	}
+
+            foreach (var channel in channelsListResponse.Items)
+            {
+                // From the API response, extract the playlist ID that identifies the list
+                // of videos uploaded to the authenticated user's channel.
+                var uploadsListId = channel.ContentDetails.RelatedPlaylists.Uploads;
+
+                var nextPageToken = "";
+                while (nextPageToken != null)
+                {
+                    var playlistItemsListRequest = youtubeService.PlaylistItems.List("snippet");
+                    playlistItemsListRequest.PlaylistId = uploadsListId;
+                    playlistItemsListRequest.MaxResults = 20;
+                    playlistItemsListRequest.PageToken = nextPageToken;
+
+                    // Retrieve the list of videos uploaded to the authenticated user's channel.
+                    var playlistItemsListResponse = playlistItemsListRequest.ExecuteAsync().Result;
+
+                    foreach (var playlistItem in playlistItemsListResponse.Items)
+                    {
+                        if (!myResults.Any(x => x.Id == playlistItem.Snippet.ResourceId.VideoId))
+                        {
+                            myResults.Add(new YouTubeVideo
+                            {
+                                Description = playlistItem.Snippet.Description,
+                                Id = playlistItem.Snippet.ResourceId.VideoId,
+                                PublishDate = playlistItem.Snippet.PublishedAt,
+                                Title = playlistItem.Snippet.Title,
+                                ThumbnailImageUrl = playlistItem.Snippet.Thumbnails.Default__.Url,
+                            });
+                        }
+                    }
+                    nextPageToken = playlistItemsListResponse.NextPageToken;
+                }
+            }
+            return myResults.AsEnumerable();
+        }
+
+
+        public IEnumerable<YouTubeVideo> GetByPlayListId(string id)
+        {
+            var playListId = id;
+            var nextPageToken = "";
+            var myResults = new List<YouTubeVideo>();
+            PlaylistItemsResource.ListRequest playlistItemsListRequest = null;
+
+            while (nextPageToken != null)
+            {
+                playlistItemsListRequest = youtubeService.PlaylistItems.List("snippet");
+                playlistItemsListRequest.PlaylistId = playListId;
+                playlistItemsListRequest.MaxResults = 10;
+                playlistItemsListRequest.PageToken = nextPageToken;
+
+                try
+                {
+                    var playlistItemsListResponse = playlistItemsListRequest.ExecuteAsync().Result;
+                    foreach (var playlistItem in playlistItemsListResponse.Items)
+                    {
+                        myResults.Add(new YouTubeVideo
+                        {
+                            Description = playlistItem.Snippet.Description,
+                            Id = playlistItem.Snippet.ResourceId.VideoId,
+                            PublishDate = playlistItem.Snippet.PublishedAt,
+                            Title = playlistItem.Snippet.Title,
+                        });
+                    }
+
+                    nextPageToken = playlistItemsListResponse.NextPageToken;
+                }
+                catch (System.AggregateException agex)
+                {
+
+                }
+            }
+            return myResults.AsEnumerable();
+        }
+    }
 }
--- YTPLApp/Scripts/app.js
+++ YTPLApp/Scripts/app.js
@@ -1,32 +1,34 @@
 ﻿angular.module('app', [])
-		.service('youTubeService', ['$http', function ($http) {
-			this.get = function (resourceUrl) {
-				return $http.get(resourceUrl);
-			}
-		}]).controller('youTubeController', ['$scope', 'youTubeService', function ($scope, service) {
-			var rootApiUrl = 'api/YouTube/'
-			$scope.results = []
 
-			$scope.youtubeQuery = '';
-			$scope.youtubeType = '';
+	.service('youTubeService', ['$http', function ($http) {
+	    this.get = function (resourceUrl) {
+	        return $http.get(resourceUrl);
+	    }
+	}])
 
-			$scope.validate = function () {
-				alert('validating');
-				if (($scope.youtubeQuery === undefined || scope.youtubeQuery === '') && ($scope.youtubeType === undefined || scope.youtubeType === '')) {
-					$scope.action_url = rootApiUrl;
-				}
-				alert('calling service');
-				service.get($scope.action_url).success(function (data, status) {
-					$scope.results = data;
-				}).error(function (data, status) {
-					alert('Error, see browser console for more details');
-					console.log(data);
-				});
-			};
-			$scope.$watch('youtubeType', function () {
-				$scope.action_url = rootApiUrl + ($scope.youtubeType == "playlist" ? "GetVideoByPlaylist/" : "GetVideosByUser/").toString() + ($scope.youtubeQuery).toString();
-			})
-			$scope.$watch('youtubeQuery', function () {
-				$scope.action_url = rootApiUrl + ($scope.youtubeType == "playlist" ? "GetVideoByPlaylist/" : "GetVideosByUser/").toString() + ($scope.youtubeQuery).toString();
-			})
-		}]);
+    .controller('youTubeController', ['$scope', 'youTubeService', function ($scope, service) {
+        var rootApiUrl = 'api/YouTube/'
+        $scope.results = []
+
+        $scope.youtubeQuery = '';
+        $scope.youtubeType = '';
+
+        $scope.submit = function () {
+            if (($scope.youtubeQuery === undefined || $scope.youtubeQuery === '') && ($scope.youtubeType === undefined || $scope.youtubeType === '')) {
+                $scope.action_url = rootApiUrl;
+            }
+            service.get($scope.action_url).success(function (data, status) {
+                $scope.results = data;
+            }).error(function (data, status) {
+                alert('Error, see browser console for more details');
+                console.log(data);
+            });
+        };
+
+        $scope.$watch('youtubeType', function () {
+            $scope.action_url = rootApiUrl + ($scope.youtubeType == "playlist" ? "GetVideoByPlaylist/" : "GetVideosByUser/").toString() + ($scope.youtubeQuery).toString();
+        })
+        $scope.$watch('youtubeQuery', function () {
+            $scope.action_url = rootApiUrl + ($scope.youtubeType == "playlist" ? "GetVideoByPlaylist/" : "GetVideosByUser/").toString() + ($scope.youtubeQuery).toString();
+        })
+    }]);
